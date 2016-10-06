4b49ecea60e678df5dfe98da674dcf3b5c10600f
Carlos Cabo <carloscabo@gmail.com>
2016-10-06 20:51:39 +0200

    Port WIP

--- src/HUSL.class.php
+++ src/HUSL.class.php
@@ -29,15 +29,15 @@ class HUSL {
  		return;
   }
 
-  private static $m = [
-    'R' => [3.2409699419045214, -1.5373831775700935, -0.49861076029300328],
-    'G' => [-0.96924363628087983, 1.8759675015077207, 0.041555057407175613],
-    'B' => [0.055630079696993609, -0.20397695888897657, 1.0569715142428786]
-  ];
+  private static $m = array(
+    'R' => array(3.2409699419045214, -1.5373831775700935, -0.49861076029300328),
+    'G' => array(-0.96924363628087983, 1.8759675015077207, 0.041555057407175613),
+    'B' => array(0.055630079696993609, -0.20397695888897657, 1.0569715142428786)
+  );
   private static  $m_inv = [
-    'X' => [0.41239079926595948, 0.35758433938387796, 0.18048078840183429],
-    'Y' => [0.21263900587151036, 0.71516867876775593, 0.072192315360733715],
-    'Z' => [0.019330818715591851, 0.11919477979462599, 0.95053215224966058]
+    'X' => array(0.41239079926595948, 0.35758433938387796, 0.18048078840183429),
+    'Y' => array(0.21263900587151036, 0.71516867876775593, 0.072192315360733715),
+    'Z' => array(0.019330818715591851, 0.11919477979462599, 0.95053215224966058)
   ];
 
   private static $refU = 0.19783000664283681;
@@ -53,8 +53,8 @@ class HUSL {
   private static function getBounds( $L ) {
     $sub1 = pow( $L + 16, 3 ) / 1560896;
     $sub2 = $sub1 > self::$epsilon ? $sub1 : $L / self::$kappa;
-    $ret = [];
-    $iterable = ['R', 'G', 'B'];
+    $ret = array();
+    $iterable = array('R', 'G', 'B');
 
     for ( $i = 0; $i < count($iterable); $i++ ) {
       $channel = $iterable[$i];
@@ -71,19 +71,19 @@ class HUSL {
         $top2 = (838422 * $m3 + 769860 * $m2 + 731718 * $m1) * $L * $sub2 - 769860 * $t * $L;
         $bottom = (632260 * $m3 - 126452 * $m2) * $sub2 + 126452 * $t;
 
-        $ret[] = [ $top1 / $bottom, $top2 / $bottom];
+        $ret[] = array( $top1 / $bottom, $top2 / $bottom );
       }
     }
     return $ret;
-  };
+  }
 
   private static function intersectLineLine( $line1, $line2 ) {
-    return ( $line1[1] - $line2[1]) / ($line2[0] - $line1[0]);
-  };
+    return ( $line1[1] - $line2[1]) / ($line2[0] - $line1[0] );
+  }
 
   private static function distanceFromPole( $point ) {
     return sqrt( pow( $point[0], 2 ) + pow( $point[1], 2 ) );
-  };
+  }
 
   private static function lengthOfRayUntilIntersect( $theta, $line ) {
     // theta  -- angle of ray starting at (0, 0)
@@ -107,16 +107,16 @@ class HUSL {
 
     $len = $b1 / ( sin( $theta ) - $m1 * cos( $theta ) );
     if ( $len < 0 ) {
-        return null;
+      return null;
     }
     return $len;
-  };
+  }
 
   // For given lightness, returns the maximum chroma. Keeping the chroma value
   // below this number will ensure that for any hue, the color is within the RGB
   // gamut.
   private static function maxSafeChromaForL( $L ) {
-    $lengths = [];
+    $lengths = array();
     $iterable = self::getBounds( $L );
     for ( $i = 0; $i < count($iterable); $i++) {
 
@@ -128,13 +128,13 @@ class HUSL {
       $lengths[] = self::distanceFromPole( [$x, $b1 + $x * $m1] );
     }
     return min( lengths );
-  };
+  }
 
   // For a given lightness and hue, return the maximum chroma that fits in
   // the RGB gamut.
   private static function maxChromaForLH( $L, $H ) {
     $hrad = $H / 360 * M_PI * 2;
-    $lengths = [];
+    $lengths = array();
     $iterable = self::getBounds( $L );
     for ( $i = 0; $i < count($iterable); $i++) {
       $line = $iterable[$i];
@@ -144,7 +144,7 @@ class HUSL {
       }
     }
     return min( $lengths );
-  };
+  }
 
   private static function dotProduct( $a, $b ) {
     $ret = 0;
@@ -152,7 +152,7 @@ class HUSL {
       $ret += $a[$i] * $b[$i];
     }
     return $ret;
-  };
+  }
 
   // Used for rgb conversions
   private static function fromLinear( $c ) {
@@ -161,23 +161,23 @@ class HUSL {
     } else {
       return 1.055 * pow( $c, 1 / 2.4 ) - 0.055;
     }
-  };
+  }
 
   private static function toLinear( $c ) {
     $a = 0.055;
     if ( $c > 0.04045 ) {
-      return pow( ( $c + $a ) / ( 1 + $a ), 2.4);
+      return pow( ( $c + $a ) / ( 1 + $a ), 2.4 );
     } else {
       return $c / 12.92;
     }
-  };
+  }
 
   public static function xyzToRgb ( $tuple ) {
     $R = self::fromLinear( self::dotProduct( self::$m['R'], $tuple ) );
     $G = self::fromLinear( self::dotProduct( self::$m['G'], $tuple ) );
     $B = self::fromLinear( self::dotProduct( self::$m['B'], $tuple ) );
-    return [ $R, $G, $B ];
-  };
+    return array( $R, $G, $B );
+  }
 
   public static function rgbToXyz( $tuple ) {
     $R = $tuple[0];
@@ -188,8 +188,8 @@ class HUSL {
     $X = self::dotProduct( self::m_inv['X'], $rgbl);
     $Y = self::dotProduct( self::m_inv['Y'], $rgbl);
     $Z = self::dotProduct( self::m_inv['Z'], $rgbl);
-    return [ $X, $Y, $Z ];
-  };
+    return array( $X, $Y, $Z );
+  }
 
   // http://en.wikipedia.org/wiki/CIELUV
   // In these formulas, Yn refers to the reference white point. We are using
@@ -208,7 +208,7 @@ class HUSL {
     } else {
       return pow( ( $L + 16 ) / 116, 3 );
     }
-  };
+  }
 
   public static function xyzToLuv( $tuple ) {
     $X = $tuple[0];
@@ -218,33 +218,32 @@ class HUSL {
     // the following two lines
 
     if ( $Y === 0 ) {
-      return [ 0, 0, 0 ];
+      return array( 0, 0, 0 );
     }
     $L = self::Y_to_L( $Y );
     $varU = 4 * $X / ( $X + 15 * $Y + 3 * $Z );
     $varV = 9 * $Y / ( $X + 15 * $Y + 3 * $Z );
     $U = 13 * $L * ( $varU - $refU );
     $V = 13 * $L * ( $varV - $refV );
-    return [ $L, $U, $V ];
-  };
+    return array( $L, $U, $V );
+  }
 
   public static function luvToXyz( $tuple ) {
-
     $L = $tuple[0];
     $U = $tuple[1];
     $V = $tuple[2];
     // Black will create a divide-by-zero error
 
     if ( $L === 0 ) {
-      return [ 0, 0, 0 ];
+      return array( 0, 0, 0 );
     }
     $varU = $U / ( 13 * $L ) + $refU;
     $varV = $V / ( 13 * $L ) + $refV;
     $Y = self::L_to_Y( $L );
     $X = 0 - 9 * $Y * $varU / ( ( $varU - 4 ) * $varV - $varU * $varV );
     $Z = ( 9 * $Y - 15 * $varV * Y - $varV * X ) / ( 3 * $varV );
-    return [ $X, $Y, $Z ];
-  };
+    return array( $X, $Y, $Z );
+  }
 
   public static function luvToLch( $tuple ) {
     $L = $tuple[0];
@@ -262,8 +261,129 @@ class HUSL {
         $H = 360 + $H;
       }
     }
-    return [ $L, $C, $H ];
-  };
+    return array( $L, $C, $H );
+  }
+
+  public static function lchToLuv( $tuple ) {
+    $L = $tuple[0];
+    $C = $tuple[1];
+    $H = $tuple[2];
+
+    $Hrad = $H / 360 * 2 * M_PI;
+    $U = cos( $Hrad ) * $C;
+    $V = sin( $Hrad ) * $C;
+    return array( $L, $U, $V );
+  }
+
+  public static function huslToLch( $tuple ) {
+    $H = $tuple[0];
+    $S = $tuple[1];
+    $L = $tuple[2];
+    // White and black: disambiguate chroma
+
+    if ( $L > 99.9999999 || $L < 0.00000001 ) {
+      $C = 0;
+    } else {
+      $max = self::maxChromaForLH( $L, $H );
+      $C = $max / 100 * $S;
+    }
+    return array( $L, $C, $H );
+  }
+
+  public static function lchToHusl( $tuple ) {
+    $L = $tuple[0];
+    $C = $tuple[1];
+    $H = $tuple[2];
+    // White and black: disambiguate saturation
+
+    if ( $L > 99.9999999 || $L < 0.00000001 ) {
+      $S = 0;
+    } else {
+      $max = self::maxChromaForLH( $L, $H );
+      $S = $C / $max * 100;
+    }
+    return array( $H, $S, $L );
+  }
+
+  //# PASTEL HUSL
+
+  public static function huslpToLch( $tuple ) {
+    $H = $tuple[0];
+    $S = $tuple[1];
+    $L = $tuple[2];
+
+    // White and black: disambiguate chroma
+    if ( $L > 99.9999999 || $L < 0.00000001) {
+      $C = 0;
+    } else {
+      $max = self::maxSafeChromaForL( $L );
+      $C = $max / 100 * S;
+    }
+    return array( $L, $C, $H );
+  }
+
+  public static function lchToHuslp( $tuple ) {
+    $L = $tuple[0];
+    $C = $tuple[1];
+    $H = $tuple[2];
+    // White and black: disambiguate saturation
+
+    if ( $L > 99.9999999 || $L < 0.00000001) {
+      $S = 0;
+    } else {
+      $max = self::maxSafeChromaForL( $L );
+      $S = $C / $max * 100;
+    }
+    return array( $H, $S, $L );
+  }
+
+  // From https://gist.github.com/Pushplaybang/5432844
+  public static function rgb2Hex( $rgb ) {
+     $hex = "#";
+     $hex .= str_pad( dechex( $rgb[0] ), 2, "0", STR_PAD_LEFT);
+     $hex .= str_pad( dechex( $rgb[1] ), 2, "0", STR_PAD_LEFT);
+     $hex .= str_pad( dechex( $rgb[2] ), 2, "0", STR_PAD_LEFT);
+
+     return $hex; // returns the hex value including the number sign (#)
+  }
+
+  public static function hexTorgb( $hex ) {
+     $hex = str_replace("#", "", $hex);
+
+     if( strlen( $hex ) == 3 ) {
+        $r = hexdec( substr( $hex,0,1 ).substr( $hex,0,1 ) );
+        $g = hexdec( substr( $hex,1,1 ).substr( $hex,1,1 ) );
+        $b = hexdec( substr( $hex,2,1 ).substr( $hex,2,1 ) );
+     } else {
+        $r = hexdec( substr( $hex,0,2 ) );
+        $g = hexdec( substr( $hex,2,2 ) );
+        $b = hexdec( substr( $hex,4,2 ) );
+     }
+     $rgb = array( $r, $g, $b );
+
+     return $rgb; // returns an array with the rgb values
+  }
+
+  // Helper functions
+
+  public static function lchToRgb( $tuple ) {
+    return self::xyzToRgb( self::luvToXyz( self::lchToLuv( $tuple ) ) );
+  }
+  public static function rgbToLch( $tuple ) {
+    return self::luvToLch( self::xyzToLuv( self::rgbToXyz( $tuple ) ) );
+  }
+  public static function huslToRgb( $tuple ) {
+    return self::lchToRgb( self::huslToLch( $tuple ) );
+  }
+  public static function rgbToHusl( $tuple ) {
+    return self::lchToHusl( self::rgbToLch( $tuple ) );
+  }
+  public static function huslpToRgb( $tuple ) {
+    return self::lchToRgb( self::huslpToLch( $tuple ) );
+  }
+  public static function rgbToHuslp( $tuple ) {
+    return self::lchToHuslp( self::rgbToLch( $tuple ) );
+  }
 
 }
 
